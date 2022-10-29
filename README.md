feed76ea37f924811e00a6e85b2f4a791f10c9ef
Abhai Sasidharan <abhaisasidharan@gmail.com>
2022-10-30 01:57:32 +0530

    Sitemap url will be loaded into memory, interval is given as argument

---
+++ README.md
@@ -0,0 +1,51 @@
+# warmer
+A CDN cache warmer in rust for the sitemap.xml files that look like this:
+
+```xml
+<?xml version="1.0" encoding="UTF-8"?>
+<?xml-stylesheet type="text/xsl" href="/sitemap.xsl"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">
+<url>
+    <loc>https://abh.ai/</loc>
+    <lastmod>2022-06-25T20:46Z</lastmod>
+    <changefreq>daily</changefreq>
+    <priority>1.0</priority>
+</url>
+<url>
+    <loc>https://abh.ai/photos/nature</loc>
+    <lastmod>2022-09-25T05:33Z</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.7</priority>
+</url>
+<url>
+    <loc>https://abh.ai/portraits</loc>
+    <lastmod>2022-09-24T18:42Z</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.7</priority>
+</url>
+</urlset>
+```
+
+## Other examples of sitemaps that work
+
+- https://abh.ai/sitemap.xml
+- https://qed42.com/sitemap.xml
+- https://www.australia.gov.au/sitemap.xml
+- https://www.alkhaleej.ae/sitemap.xml?page=1
+- https://www.axelerant.com/sitemap.xml
+- https://ffw.com/sitemap.xml
+
+## Usage
+
+Download (from [here](https://github.com/codingsasi/warmer/releases)) and run the executable binary on linux with the following command
+```
+warmer http(s)://someurl.com/sitemap.xml interl
+ - warmer https://abh.ai/sitemap.xml 5
+ - warmer https://abh.ai/sitemap.xml 1
+```
+- The interval value should be specified in seconds, and it's how long the warmer waiting before loading the next URL in the sitemap.
+- The url should be specified with scheme as well.
+
+### Notes
+Large sitemaps, that include other zipped or gzipped sitemaps are not supported yet. I'll release that as and when I get time. But for most sitemaps this should warm it just fine.
+Currently on supported on 64-bit linux OS.
--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,77 @@
+extern crate core;
+
 use serde::{Deserialize, Serialize};
 use serde_xml_rs::{from_str};
-use std::fs;
-use curl::easy::Easy;
 use std::{thread, time};
+use isahc::{config::RedirectPolicy, prelude::*, Request};
+use std::env;
 
+/// The struct to deserialize and hold the items in <url></url>
+/// in the sitemap.xml
+/// <url>
+///     <loc>https://abh.ai/</loc>
+///     <lastmod>2022-06-25T20:46Z</lastmod>
+///     <changefreq>daily</changefreq>
+///     <priority>1.0</priority>
+/// </url>
 #[derive(Debug, Serialize, Deserialize, PartialEq)]
 struct Url {
     loc: String,
     #[serde(default = "default_lastmod")]
     lastmod: String,
+    #[serde(default = "default_changefreq")]
     changefreq: String,
     #[serde(default = "default_priority")]
     priority: String,
 }
 
+/// The struct to hold the urlset items in the sitemap.xml.
 #[derive(Debug, Serialize, Deserialize, PartialEq)]
 struct UrlSet {
     url: Vec<Url>
 }
 
 fn main() {
-    let src = fs::read_to_string("sitemap.xml").unwrap();
+    let args: Vec<String> = env::args().collect();
+    let mut interval = "5";
+    if args.len() > 1 {
+        interval = &args[2];
+    }
+    let mut response = Request::get(&args[1])
+        .redirect_policy(RedirectPolicy::Follow)
+        .body(()).unwrap()
+        .send().unwrap();
+    if response.status().as_str() != "200" {
+        panic!("The Sitemap URL returned non 200 status.");
+    }
+    let src = response.text().unwrap();
+    println!("The sitemap was loaded successfully!");
     let us: UrlSet = from_str(src.as_str()).unwrap();
     for url in us.url.iter() {
         println!("{:?}", url.loc);
-        let mut easy = Easy::new();
-        easy.url(&url.loc).unwrap();
-        easy.write_function(|data| {
-            Ok(data.len())
-        }).unwrap();
-        easy.perform().unwrap();
-
-        println!("{}", easy.response_code().unwrap());
-        thread::sleep(time::Duration::from_secs(5));
+        let page = Request::get(&url.loc)
+            .redirect_policy(RedirectPolicy::Follow)
+            .body(()).unwrap()
+            .send().unwrap();
+        println!("{}", page.status().as_str());
+        thread::sleep(time::Duration::from_secs(interval.parse::<u64>().unwrap()));
     }
 }
 
+/// Returns a static string to use in place of missing
+/// lastmod tag in the xml structure.
 fn default_lastmod() -> String {
     "2021-12-28T08:37Z".to_string()
 }
 
+/// Returns a static string to use in place of missing
+/// priority tag in the xml structure.
 fn default_priority() -> String {
     "0.5".to_string()
+}
+
+/// Returns a static string to use in place of missing
+/// changefreq tag in the xml structure.
+fn default_changefreq() -> String {
+    "daily".to_string()
 }
